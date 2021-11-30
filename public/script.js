f438bb14d7f291f03b24eb3ce237262952c5e583
Khutornyi_Taras <tarcom18@gmail.com>
2021-11-30 20:06:58 +0200

    init commit

---
+++ public/script.js
@@ -0,0 +1,182 @@
+$(document).ready(function () {
+    const view = new MainView();
+    const model = new MainModel();
+    const controller = new MainController(model, view);
+    controller.addHandle();
+    controller.findMyLocation();
+    controller.showMyCities();
+
+    $(document).mouseup(function (e) {
+        var div = $("#listForm");
+        if (!div.is(e.target)
+            && div.has(e.target).length === 0) {
+            $("#cities_found").hide();
+        }
+    });
+})
+
+class MainView {
+    constructor() {
+        this.$findButton = $("#findButton");
+        this.$currentPosition = $("#current_position");
+        this.$addCity;
+        this.$deleteCity = [];
+    }
+
+    showCities(array) {
+        let $cities = $("#cities_found");
+        $cities.html("");
+        $cities.show();
+        if (array.length === 0) {
+            let li = $("<li></li>").text("No results found");
+            $cities.append(li);
+        }
+
+        else for (let i = 0; i < array.length; i++) {
+            let flag = false;
+            let li = $("<li></li>").text(array[i].name + ", " + (array[i].state !== "" ? array[i].state + ", " : "") + array[i].country);
+            let button = $(`<button id=${array[i].id} class='addCity'></button>`).text("Add");
+            for (let j = 0; j < this.$deleteCity.length; j++) {
+                if (array[i].id === Number.parseInt(this.$deleteCity[j].getAttribute("class").split(' ')[0])) {
+                    flag = true;
+                }
+            }
+            if (flag === false) li.append(button);
+            $cities.append(li);
+            this.$addCity = $('.addCity');
+        }
+    }
+
+    showMyCities(array) {
+        let $myCities = $("#my_cities");
+        $myCities.html("");
+        if (array.length === 0) {
+            let li = $("<li></li>").text("Add some cities to show");
+            $myCities.append(li);
+        }
+
+        else for (let i = 0; i < array.length; i++) {
+            let li = $("<li></li>").html(`${array[i].city}, ${array[i].country}</br>Weather: ${array[i].weather}</br>Temperature: ${array[i].temperature}</br>Wind speed: ${array[i].wind}</br>`);
+            let buttonDelete = $(`<button class="${array[i].id} deleteButton"></button>`).text("Delete");
+            li.append(buttonDelete);
+            $myCities.append(li);
+            this.$deleteCity = $('.deleteButton');
+        }
+    }
+
+    showMyWeather(myWeather) {
+        let $myPostion = $("#current_position");
+        $myPostion.html(`${myWeather.city}, ${myWeather.country}</br>Weather: ${myWeather.weather}</br>Temperature: ${myWeather.temperature}</br>Wind speed: ${myWeather.wind}</br>`);
+    }
+}
+
+class MainController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+        this.findCities = this.findCities.bind(this);
+        this.addCity = this.addCity.bind(this);
+        this.deleteCity = this.deleteCity.bind(this);
+    }
+
+    addHandle() {
+        this.view.$findButton.on("click", this.findCities);
+    }
+
+    async findCities() {
+        let text = $("#inputCity").val();
+        if (text !== "") {
+            let array = await this.model.findCities(text);
+            this.view.showCities(array);
+            if (array.length !== 0) this.view.$addCity.on("click", this.addCity);
+        }
+    }
+
+    async addCity(e) {
+        let id = e.target.id;
+        await this.model.addCity(id);
+        this.showMyCities();
+        $(e.target).css('display', 'none');
+    }
+
+    async deleteCity(e) {
+        let id = e.target.getAttribute("class").split(' ')[0];
+        await this.model.deleteCity(id);
+        this.showMyCities();
+    }
+
+    async findMyLocation() {
+        let [lat, lon] = await this.model.findMe();
+        let myWeather = await this.model.myWeather(lat, lon);
+        this.view.showMyWeather(myWeather);
+    }
+
+    async showMyCities() {
+        let array = await this.model.getMyCitiesWeather();
+        this.view.showMyCities(array);
+        if (array.length > 0) this.view.$deleteCity.on("click", this.deleteCity);
+    }
+}
+
+class MainModel {
+    constructor() {
+        this.url = 'http://localhost:3000/';
+    }
+
+    findCities(name) {
+        return fetch(this.url + "city/" + name[0].toUpperCase() + name.slice(1))
+            .then((response) => response.json())
+            .then((text) => text.result);
+    }
+
+    findMe() {
+        return new Promise(function (resolve) {
+            $.ajax({
+                url: "https://geolocation-db.com/jsonp",
+                jsonpCallback: "callback",
+                dataType: "jsonp",
+                success: function (location) {
+                    let lat = location.latitude;
+                    let lon = location.longitude;
+                    resolve([lat, lon]);
+                }
+            });
+        });
+    }
+
+    myWeather(lat, lon) {
+        return fetch(this.url + lat + ',' + lon)
+            .then((response) => response.json());
+    }
+
+    async addCity(id) {
+        try {
+            let result = await fetch(this.url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ id: id })
+            });
+            let json = await result.json();
+            return json;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    async deleteCity(id) {
+        try {
+            let result = await fetch(this.url + id, {
+                method: 'DELETE',
+            });
+            let json = await result.json();
+            return json;
+        } catch (err) {
+            return err;
+        }
+    }
+
+    getMyCitiesWeather() {
+        return fetch(this.url + "myCities/")
+            .then((response) => response.json());
+    }
+}
---
+++ server.js
@@ -0,0 +1,115 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import fs from 'fs';
+import bp from 'body-parser';
+
+let app = express();
+app.use(cors());
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+let db;
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const API_key = "d43125b9a05afc062bda72694d1ea670";
+
+client.connect(err => {
+    if (err) return console.log(err);
+    app.listen(3000, () => console.log('API started'));
+});
+
+app.get('/city/:name', (req, res) => {
+    db = client.db("allCities");
+    db.collection('cities').count(function (err, count) {
+        if (!err && count === 0) {
+            let cityData = fs.readFileSync('city.list.json');
+            let cities = JSON.parse(cityData);
+            db.collection('cities').insertMany(cities);
+        }
+    });
+    db.collection('cities').find({ "name": req.params.name }).toArray((err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.json({ result });
+    });
+});
+
+app.get('/:lat,:lon', async (req, res) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.params.lat}&lon=${req.params.lon}&appid=${API_key}&units=metric`;
+    const response = await fetch(url)
+        .then(res => res.json())
+        .catch(e => {
+            console.error({
+                "message": "OH NO",
+                error: e,
+            })
+        })
+    res.json({
+        "city": response.name,
+        "country": response.sys.country,
+        "weather": response.weather[0].description,
+        "temperature": Math.round(response.main.temp),
+        "wind": Math.round(response.wind.speed)
+    });
+});
+
+app.get("/myCities", (req, res) => {
+    let myCityArray = [];
+    db = client.db("myCities");
+    db.collection('cities').find().toArray(async (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+
+        for (let i = 0; i < result.length; i++) {
+            const url = `https://api.openweathermap.org/data/2.5/weather?id=${result[i].id}&appid=${API_key}&units=metric`;
+            const response = await fetch(url)
+                .then(res => res.json())
+                .catch(e => {
+                    console.error({
+                        "message": "OH NO",
+                        error: e,
+                    })
+                })
+            myCityArray.push({
+                "id": response.id,
+                "city": response.name,
+                "country": response.sys.country,
+                "weather": response.weather[0].description,
+                "temperature": Math.round(response.main.temp),
+                "wind": Math.round(response.wind.speed)
+            });
+        }
+        res.send(myCityArray);
+    });
+});
+
+app.post('/', (req, res) => {
+    db = client.db("myCities");
+    let city = {
+        id: req.body.id
+    };
+    db.collection('cities').insertOne(city, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.status(200).json(city);
+    })
+});
+
+app.delete('/:id', (req, res) => {
+    db = client.db("myCities");
+    db.collection('cities').deleteOne({ "id": req.params.id }, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.status(200).json(result);
+    });
+});
